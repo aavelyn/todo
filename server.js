0bfe29e2bc1939d1185fc37b17ace0f7fca95fb7
Han <hanche2001@gmail.com>
2016-05-18 14:05:05 +0200

    add deleted

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var bodyParser = require('body-parser');
 var _ = require('underscore');
 
 
-var app = express()
+var app = express();
 var PORT = process.env.PORT || 3000;
 var todos =[];
 var todoNextId=1;
@@ -12,11 +12,11 @@ app.use(bodyParser.json());
 
 app.get('/',function(req,res){
 	res.send('Todo API Root')
-})
+});
 
 app.get('/todos',function(req,res){
 	res.json(todos);
-})
+});
 
 app.get('/todos/:id',function(req,res){
 	var todoId= parseInt(req.params.id, 10);
@@ -27,7 +27,7 @@ app.get('/todos/:id',function(req,res){
 	} else{
 		res.status(404).send();
 	}
-})
+});
 
 
 // POST /todos:
@@ -49,8 +49,21 @@ app.post('/todos', function(req,res){
 });
 
 
+// Delete /Todos:
 
+app.delete('/todos/:id',function (req,res) {
+	var todoId= parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos,{id:todoId});
+
+	if(!matchedTodo){
+		res.status(400).json({"error":"ID not found"})
+	}
+
+	todos = _.without(todos,matchedTodo);
+
+	res.json(matchedTodo);
+});
 
 app.listen(PORT, function(){
 	console.log('Server Started on Port: ' + PORT)
-})
+});
