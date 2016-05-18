225f836bd7056281830308e0679e143b1f1072a8
Han <hanche2001@gmail.com>
2016-05-18 15:16:15 +0200

    added filter for completed

--- server.js
+++ server.js
@@ -15,7 +15,16 @@ app.get('/',function(req,res){
 });
 
 app.get('/todos',function(req,res){
-	res.json(todos);
+	var queryParams = req.query;
+	var filterTodos = todos;
+
+	if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true'){
+		filterTodos = _.where(filterTodos,{completed:true});
+	} else if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'false'){
+		filterTodos = _.where(filterTodos,{completed:false});
+	}
+
+	res.json(filterTodos);
 });
 
 app.get('/todos/:id',function(req,res){
@@ -38,8 +47,6 @@ app.post('/todos', function(req,res){
 	if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
 		return res.status(400).send();
 	}
-
-
 	body.id = todoNextId++;
 	body.description = body.description.trim();
 
